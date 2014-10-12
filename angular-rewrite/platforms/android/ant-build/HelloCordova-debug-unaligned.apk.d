a6030c3986f8e3e854e817606e02a36d33fdd72c
Michael Wager <mail@mwager.de>
2014-10-12 20:53:43 +0200

    remove angular files from dev

--- angular-rewrite/platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Volumes/Data/htdocs/myApp/platforms/android/ant-build/HelloCordova-debug-unaligned.apk : \
-/Volumes/Data/htdocs/myApp/platforms/android/ant-build/HelloCordova.ap_ \
-/Volumes/Data/htdocs/myApp/platforms/android/ant-build/classes.dex \
-/Volumes/Data/htdocs/myApp/platforms/android/CordovaLib/ant-build/classes.jar \
